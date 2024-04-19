3a9d535792fdfc8a475d14c791252f85002a6903
Rizky Rafi Azhara <azhararizkyrafi@gmail.com>
2024-04-19 14:30:04 +0700

    refactor: :construction: Update index for under development alert

---
+++ src/components/main/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Main = () => {
+  return (
+    <main className='bg-black h-screen flex justify-center items-center'>
+      <h1 className='text-white text-5xl text-center font-bold'>This Website is Under Development.</h1>
+    </main>
+  );
+};
+
+export default Main;
