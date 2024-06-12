a6e42bfeac1ff2577d4c6298b504026b7716ed27
Rizky Rafi Azhara <azhararizkyrafi@gmail.com>
2024-06-12 14:30:58 +0700

    refactor: :recycle: Update dependencies, install shadcn, and refactor jsx to tsx

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import { Button } from "../ui/button";
+
 
 const Main = () => {
   return (
-    <main className='bg-black h-screen flex justify-center items-center'>
+    <main className='bg-black h-screen flex flex-col justify-center items-center gap-2'>
       <h1 className='text-white text-5xl text-center font-bold'>This Website is Under Development.</h1>
     </main>
   );
