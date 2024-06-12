b4a90fad8a9601cb118ab906828218e59b511534
Rizky Rafi Azhara <azhararizkyrafi@gmail.com>
2024-06-12 14:37:00 +0700

    refactor: :art: Update main and layout components to improve website appearance and structure

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -3,8 +3,8 @@ import { Button } from "../ui/button";
 
 const Main = () => {
   return (
-    <main className='bg-black h-screen flex flex-col justify-center items-center gap-2'>
-      <h1 className='text-white text-5xl text-center font-bold'>This Website is Under Development.</h1>
+    <main className='flex flex-col justify-center items-center gap-2'>
+      <h1 className='text-black text-5xl text-center font-bold'>This Website is Under Development.</h1>
     </main>
   );
 };
