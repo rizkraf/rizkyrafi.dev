aa5f28ed6f17d5883da1032e1932ebf63184634d
Rizky Rafi Azhara <azhararizkyrafi@gmail.com>
2024-06-13 14:04:34 +0700

    feat: :sparkles: Add project section in work page

---
+++ src/data/projects.ts
@@ -0,0 +1,45 @@
+const PROJECTS: {
+  name: string
+  description: string
+  previewImage: string
+  repoUrl: string
+  liveLink: string
+}[] = [
+  {
+    name: 'Quran Web',
+    description: 'Lorem ipsum dolor sit amet',
+    liveLink: 'https://quran.rizkyrafi.id/',
+    previewImage: '/Quran-Web.png',
+    repoUrl: 'https://github.com/rizkraf/quran-web',
+  },
+  {
+    name: 'Noter',
+    description: 'Lorem ipsum dolor sit amet',
+    liveLink: 'https://noter.rizkyrafi.id/',
+    previewImage: '/Noter.png',
+    repoUrl: 'https://github.com/rizkraf/noter',
+  },
+  {
+    name: 'Bookshelf',
+    description: 'Lorem ipsum dolor sit amet',
+    liveLink: 'https://bookshelf-website.netlify.app/',
+    previewImage: '/Bookshelf.png',
+    repoUrl: 'https://github.com/rizkraf/bookshelf-apps',
+  },
+  {
+    name: 'Spense',
+    description: 'Lorem ipsum dolor sit amet',
+    liveLink: 'https://spense-web.netlify.app/',
+    previewImage: '/Spense.png',
+    repoUrl: 'https://github.com/rizkraf/spense',
+    },
+    {
+      name: 'Pong Game',
+      description: 'Lorem ipsum dolor sit amet',
+      liveLink: '/',
+      previewImage: '/Pong.png',
+      repoUrl: 'https://github.com/rizkraf/Rizky_Rafi_Azhara-Pong',
+    },
+]
+
+export default PROJECTS
