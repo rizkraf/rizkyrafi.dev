d88cd9014bf4756e6a50b0fb536f317f5ad68567
Rizky Rafi Azhara <azhararizkyrafi@gmail.com>
2024-06-13 10:56:04 +0700

    feat: :sparkles: Add Experience component to display past roles in about page

---
+++ src/data/experience.ts
@@ -0,0 +1,18 @@
+const PAST_ROLES: {
+  company: string;
+  role: string;
+  description: string;
+  startDate: string;
+  endDate: string;
+}[] = [
+  {
+    company: 'Roketin',
+    role: 'Frontend Engineer',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam vel hic aspernatur molestiae exercitationem rerum reiciendis mollitia magnam harum ipsum?',
+    startDate: 'September 2023',
+    endDate: 'Present',
+  },
+];
+
+export default PAST_ROLES;
