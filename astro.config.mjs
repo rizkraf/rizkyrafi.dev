4a9e9703c04501da137ef28ea794625e85229956
Rizky Rafi Azhara <azhararizkyrafi@gmail.com>
2024-06-13 21:24:12 +0700

    feat: :wrench: Add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,18 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
 import react from '@astrojs/react';
+
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://rizkyrafi.id',
   integrations: [
     tailwind({
       applyBaseStyles: false,
     }),
     react(),
+    sitemap(),
   ],
   trailingSlash: 'always',
   prefetch: {
