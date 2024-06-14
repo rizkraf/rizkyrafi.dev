68e8f147cec71510b54bde0a8553fbe746de78e0
Rizky Rafi Azhara <azhararizkyrafi@gmail.com>
2024-06-14 14:19:25 +0700

    feat: :wrench: Add @playform/inline npm dependency for inline rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,9 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
-
 import vercel from '@astrojs/vercel/static';
+
+import playformInline from '@playform/inline';
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,6 +15,7 @@ export default defineConfig({
     }),
     react(),
     sitemap(),
+    (await import('@playform/inline')).default(),
   ],
   trailingSlash: 'always',
   prefetch: {
